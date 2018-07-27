c1db8308cc6f4b41ed9e42461e131c8202088704
Rayan Castro <rayancdc@gmail.com>
2018-07-27 11:24:34 -0300

    Add models cocktail, ingrediant and dose

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "#{Ingredient.count} ingredients in database"
+open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list") do |response|
+  puts "Generating seeds..."
+  response.each do |string|
+    JSON.parse(string).each do |element|
+      Ingredient.create!(name: element["strIngredient1"])
+    end
+  end
+end
+
+puts "#{Ingredient.count} ingredients in database"
+
+
+# [0].each do |result|
+#         Ingredient.create!(name: result["strIngredient1"])
+#       end
